8b273643da9df813962664ec849a8b04553fe0cd
Thomas Dissert <thomasdissert>
2018-10-18 11:02:50 +0200

    added passport code

--- server/app.js
+++ server/app.js
@@ -1,65 +1,87 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
 
-//connect MongoDB
 mongoose.connect('mongodb://localhost/studivision', { useCreateIndex: true, useNewUrlParser: true });
+var db = mongoose.connection;
 
-var User = require('./models/User.js')
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
+// Init App
 var app = express();
 
-//Middleware
-app.use( bodyParser.json({ type: 'application/json' }) );
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
-// root route
-app.get('/', function (req, res) {
-  console.log('Inside GET / callback function');
-  res.send('Root');
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-// login route
-app.get('/login', (req, res) => {
-  console.log('Inside GET /login callback function');
-  res.status(200).send("Please login");
-})
 
-app.post('/login', (req, res) => {
-  console.log('Inside GET /login callback function');
-  console.log(req.body.email);
-  console.log(req.body.password);
-  User.findById(req.body.email, function (err, user) {
-    if (err) {
-      res.status(400).send(err);
-    }
-    else {
-      res.status(200).send(user);
-    }
-  })
-})
 
-// register route
-app.get('/register', (req, res) => {
-  console.log('Inside GET /register callback function');
-  res.send(`Please register!\n`);
-})
+app.use('/', routes);
+app.use('/users', users);
 
-app.post('/register', (req, res) => {
-  console.log('Inside GET /register callback function');
-  console.log(req.body.user);
-  console.log(req.body.email);
-  console.log(req.body.password);
-  User.create(req.body, function (err, post) {
-    if (err) {
-      res.status(400).send("Failed POST Request");
-    }
-    else {
-      res.status(400).json(post);
-    }
-  })
-})
+// Set Port
+app.set('port', (process.env.PORT || 5000));
 
-// listen on port 5000
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,46 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
 
-// create a schema
-var userSchema = new Schema({
-  name: { type: String},
-  surname: { type: String},
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  admin: Boolean,
-  created_at: Date
+// user schema
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index:true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	}
 });
 
-// the schema is useless so far
-// we need to create a model using it
-var User = mongoose.model('User', userSchema);
+var User = module.exports = mongoose.model('User', UserSchema);
 
-// make this available to our users in our Node applications
-module.exports = User;
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
